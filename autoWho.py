78d4fff3dd81058e9b0bb58945dd8ba97a206915
jsmit260 <josh.smith.it@gmail.com>
2015-04-15 11:24:02 -0400

    changed some spacing

--- autoWho.py
+++ autoWho.py
@@ -16,9 +16,8 @@ import subprocess
 if re.match(r'^((([2][5][0-5]|([2][0-4]|[1][0-9]|[0-9])?[0-9])\.){3})([2][5][0-5]|([2][0-4]|[1][0-9]|[0-9])?[0-9])$',sys.argv[1]) is None:
     	sys.exit("Invalid IPv4 format")
  
-if re.match(r'^(((25[0-5]|2[0-4][0-9]|19[0-1]|19[3-9]|18[0-9]|17[0-1]|17[3-9]|1[0-6][0-9]|1[1-9]|[2-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]))|(192\.(25[0-5]|2[0-4][0-9]|16[0-7]|169|1[0-5][0-9]|1[7-9][0-9]|[1-9][0-9]|[0-9]))|(172\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|1[0-5]|3[2-9]|[4-9][0-9]|[0-9])))\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$',sys.argv[1]) is None:
-    	print '\n\nYou can NOT use RFC1918 address "'+sys.argv[1]+'", as no WHOIS record exists.\t:)\n '
-        sys.exit("\nPlease enter a public IP address. :)\n\n ")
+if re.match(r'^(((25[0-5]|2[0-4][0-9]|19[0-1]|19[3-9]|18[0-9]|17[0-1]|17[3-9]|1[0-6][0-9]|1[1-9]|[2-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]))|(192\.(25[0-5]|2[0-4][0-9]|16[0-7]|169|1[0-5][0-9]|1[7-9][0-9]|[1-9][0-9]|[0-9]))|(172\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|1[0-5]|3[2-9]|[4-9][0-9]|[0-9])))\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$',sys.argv[1]) is None: print ('\n\nYou can NOT use RFC1918 address "'+sys.argv[1]+'", as no WHOIS record exists.\t:)\n ')
+sys.exit("\nPlease enter a public IP address.\n")
  
 #COUNTRIES
  
@@ -44,18 +43,18 @@ with open('out.json', 'r') as f :
  
     	IP = file[1].split(':')
     	if IP[1]:
-            	nextIP = IP[1].replace('"','').strip()
-            	finalIP = nextIP.replace(',','').strip()
+            nextIP = IP[1].replace('"','').strip()
+            finalIP = nextIP.replace(',','').strip()
  
     	Nation = file[5].split(':')
     	if Nation:
-            	nextNation = Nation[1].replace('"', '').strip()
-                finalNation = nextNation.replace(",",'').strip()
+			nextNation = Nation[1].replace('"','').strip()
+            finalNation = nextNation.replace(",",'').strip()
  
     	Org = file[7].split(':')
     	if Org:
-            	nextOrg= Org[1].replace('"','').strip()
-            	finalOrg= nextOrg.replace(",",'').strip()
+			nextOrg= Org[1].replace('"','').strip()
+            finalOrg= nextOrg.replace(",",'').strip()
  
  
     	sentence = ('%s (%s/%s)') %(finalIP,finalOrg,finalNation)
@@ -65,15 +64,13 @@ with open('out.json', 'r') as f :
 #for key,value in countries.items():
  
     	if finalNation in countries:
- 
-            	fullCountryName = countries[finalNation]
-            	sentence2 = ('%s (%s/%s)') %(finalIP,finalOrg,fullCountryName)
- 
-            	print ''
-            	print sentence2
-            	print ''
+			fullCountryName = countries[finalNation]
+            sentence2 = ('%s (%s/%s)') %(finalIP,finalOrg,fullCountryName)
+			print ''
+            print sentence2
+            print ''
  
     	else :
-            	print sentence
+            print sentence
 
 
